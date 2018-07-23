09054c459b5a8398b46241fa4373d6d6e9101e7e
Dekatron <onigbindeayomide@gmail.com>
2018-07-23 22:50:45 +0100

    changed command description

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -2,16 +2,10 @@
 import program from 'commander'
 import {exec} from 'child_process'
 
-
-
-let logInfo = (argument: any) => {
-  console.log(argument);
-}
-
   program
     .version('0.0.1')
     .command(' [directory]')
-    .description('List files and folders')
+    .description('Show all commands, their flags and their summary')
     .option('-h', '--help', 'Display the help instructions')
     .option('-v', '--version', 'Displays the version of the package the user is running')
     .option('--no-lockfile', 'Prevents creation and or reading of a lockfile')
